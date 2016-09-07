85905e325efd86d7e71e754dc663f6464a2b921d
Austin Soohoo <austin.soohoo@gmail.com>
2016-09-06 18:51:42 -0700

    AutoAlign Routine fixes

--- src/org/usfirst/frc/team5026/robot/commands/RoutineAutoAlign.java
+++ src/org/usfirst/frc/team5026/robot/commands/RoutineAutoAlign.java
@@ -1,8 +1,7 @@
 package org.usfirst.frc.team5026.robot.commands;
 
-import org.usfirst.frc.team5026.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -10,10 +9,35 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class RoutineAutoAlign extends CommandGroup {
     
     public RoutineAutoAlign() {
-    	double speed = 0.2;
+    	double speed = 0.18;
     	addSequential(new VisionAngleCalculation());
     	//Robot.rotate.offsetAngle = 30;
+    	double delay = 0.55;
         addSequential(new VisionAngleRotation(speed));
+        addSequential(new WaitCommand(delay));
+        addSequential(new VisionAngleCalculation());
+        addSequential(new VisionAngleRotation(speed));
+        addSequential(new WaitCommand(delay));
+        addSequential(new VisionAngleCalculation());
+        addSequential(new VisionAngleRotation(speed));
+        
+        addSequential(new VisionMoveToOptimal());
+        addSequential(new WaitCommand(delay));
+        addSequential(new VisionMoveToOptimal());
+        addSequential(new WaitCommand(delay));
+        addSequential(new VisionMoveToOptimal());
+        
+        addSequential(new VisionAngleCalculation());
+        addSequential(new VisionAngleRotation(speed));
+        addSequential(new WaitCommand(delay));
+        addSequential(new VisionAngleCalculation());
+        addSequential(new VisionAngleRotation(speed));
+        addSequential(new WaitCommand(delay));
+        addSequential(new VisionAngleCalculation());
+        addSequential(new VisionAngleRotation(speed));
+        
+        
+        
     	//addSequential(new VisionAngleCoarseRotation()); //Without Gyro
         
         //addSequential(new VisionAngleCalculation());
