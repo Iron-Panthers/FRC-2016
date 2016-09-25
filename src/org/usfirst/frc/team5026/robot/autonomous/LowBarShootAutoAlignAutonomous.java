a0f12dfe48c02dbbaa27e27caeebd87121fd5554
Adam Noworolski <adamznow@gmail.com>
2016-09-25 07:27:03 -0700

    Some auto changes, please revert first

--- src/org/usfirst/frc/team5026/robot/autonomous/LowBarShootAutoAlignAutonomous.java
+++ src/org/usfirst/frc/team5026/robot/autonomous/LowBarShootAutoAlignAutonomous.java
@@ -2,13 +2,12 @@ package org.usfirst.frc.team5026.robot.autonomous;
 
 import org.usfirst.frc.team5026.robot.Robot;
 import org.usfirst.frc.team5026.robot.commands.DriveForwardsCarveRight;
-import org.usfirst.frc.team5026.robot.commands.DriveFowardsForTime;
 import org.usfirst.frc.team5026.robot.commands.DriveRotateThetaWithGyro;
 import org.usfirst.frc.team5026.robot.commands.IntakeArmLower;
 import org.usfirst.frc.team5026.robot.commands.IntakeArmRaise;
 import org.usfirst.frc.team5026.robot.commands.IntakeRollerSpinIn;
 import org.usfirst.frc.team5026.robot.commands.IntakeRollerStop;
-import org.usfirst.frc.team5026.robot.commands.RoutineAutoAlignNoDistance;
+import org.usfirst.frc.team5026.robot.commands.RoutineAutoAlign;
 import org.usfirst.frc.team5026.robot.commands.ShooterShootRPM;
 import org.usfirst.frc.team5026.robot.commands.ShooterSlowStop;
 import org.usfirst.frc.team5026.robot.commands.ShooterWaitForStabilize;
@@ -31,15 +30,15 @@ public class LowBarShootAutoAlignAutonomous extends CommandGroup {
         addSequential(new DriveForwardsCarveRight(4));
         addSequential(new StageTwoIntake());
         addSequential(new StageTwoPulseBack());
-        addSequential(new ShooterShootRPM(Robot.rpmUpperShooter, Robot.rpmLowerShooter)); // COMMENT ME IF I DO NOT WORK AS I SHOULD
+        addSequential(new ShooterShootRPM(Robot.rpmUpperShooterAuto, Robot.rpmLowerShooterAuto)); // COMMENT ME IF I DO NOT WORK AS I SHOULD
         addSequential(new IntakeRollerStop());
         addSequential(new IntakeArmRaise());
         addSequential(new DriveRotateThetaWithGyro(15));
         //addSequential(new DriveFowardsForTime(1));
-        addSequential(new RoutineAutoAlignNoDistance());
-        addSequential(new RoutineAutoAlignNoDistance());
+        addSequential(new RoutineAutoAlign());
+        addSequential(new RoutineAutoAlign());
         //addSequential(new RoutineAutoAlign());
-        addSequential(new ShooterWaitForStabilize(Robot.rpmUpperShooter, Robot.rpmLowerShooter));
+        addSequential(new ShooterWaitForStabilize(Robot.rpmUpperShooterAuto, Robot.rpmLowerShooterAuto));
         addSequential(new StageTwoQueueToShooter());
         addSequential(new ShooterSlowStop());
     }
