76730b7418f312fe0f73a4cbeff3c55a651fff3a
Adam Noworolski <adamznow@gmail.com>
2016-10-03 07:19:03 -0700

    Feasbility test now rotates based off of our offset from start to finish

--- src/org/usfirst/frc/team5026/robot/autonomous/MultiballFeasabilityTest.java
+++ src/org/usfirst/frc/team5026/robot/autonomous/MultiballFeasabilityTest.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5026.robot.autonomous;
 
+import org.usfirst.frc.team5026.robot.Robot;
 import org.usfirst.frc.team5026.robot.commands.AutoMultiballLowBarTest;
 import org.usfirst.frc.team5026.robot.commands.DriveForwardsForTime;
 import org.usfirst.frc.team5026.robot.commands.DriveRotateThetaWithGyro;
@@ -14,14 +15,18 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MultiballFeasabilityTest extends CommandGroup {
     
     public  MultiballFeasabilityTest() {
-        addParallel(new AutoMultiballLowBarTest()); // Resets and calibrates Gyro, also tells us how much we are off by when we return
+        double offsetReturnAngle = 0;
+    	addParallel(new AutoMultiballLowBarTest()); // Resets and calibrates Gyro, also tells us how much we are off by when we return
         addParallel(new RoutineIntakeBall()); // Grabs dat ball (Shouldn't be one here though)
         addParallel(new IntakeArmLower()); // Drops arm too, does all this in parallel ot save time
         addSequential(new DriveForwardsForTime(-0.5)); // Now we drive up to the line and grab it
         addSequential(new DriveForwardsForTime(3)); // Go over bar
-        addSequential(new DriveRotateThetaWithGyro(180,0.4)); // Do a u turn (at 0.4 speed)
+        offsetReturnAngle = Robot.rotate.getGyro();
+        addSequential(new DriveRotateThetaWithGyro(180 + offsetReturnAngle,0.4)); // Do a u turn (at 0.4 speed)
         addParallel(new AutoMultiballLowBarTest()); // Resets and calibrates gyro cause we used gyro for our turn
         addSequential(new DriveForwardsForTime(3)); // Back over the low bar
+        offsetReturnAngle = Robot.rotate.getGyro();
+        addSequential(new DriveRotateThetaWithGyro(offsetReturnAngle, 0.4));
         addParallel(new RoutineIntakeBall()); // Grabs the ball (hopefully) near that area
     }
 }
