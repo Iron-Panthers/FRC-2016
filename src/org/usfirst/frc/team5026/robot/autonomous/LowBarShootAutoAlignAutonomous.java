29e05f800f1c34f61dd5f75d97f0fc7d9de5cefe
Adam Noworolski <adamznow@gmail.com>
2016-10-06 17:33:41 -0700

    Auto change to Maker Faire shot

--- src/org/usfirst/frc/team5026/robot/autonomous/LowBarShootAutoAlignAutonomous.java
+++ src/org/usfirst/frc/team5026/robot/autonomous/LowBarShootAutoAlignAutonomous.java
@@ -1,14 +1,13 @@
 package org.usfirst.frc.team5026.robot.autonomous;
 
 import org.usfirst.frc.team5026.robot.Constants;
-import org.usfirst.frc.team5026.robot.Robot;
 import org.usfirst.frc.team5026.robot.commands.DriveForwardsCarveRight;
 import org.usfirst.frc.team5026.robot.commands.DriveRotateThetaWithGyro;
 import org.usfirst.frc.team5026.robot.commands.IntakeArmLower;
 import org.usfirst.frc.team5026.robot.commands.IntakeArmRaise;
 import org.usfirst.frc.team5026.robot.commands.IntakeRollerSpinIn;
 import org.usfirst.frc.team5026.robot.commands.IntakeRollerStop;
-import org.usfirst.frc.team5026.robot.commands.RoutineAutoAlignRPMs;
+import org.usfirst.frc.team5026.robot.commands.RoutineAutoAlign;
 import org.usfirst.frc.team5026.robot.commands.ShooterShootRPM;
 import org.usfirst.frc.team5026.robot.commands.ShooterSlowStop;
 import org.usfirst.frc.team5026.robot.commands.ShooterWaitForStabilize;
@@ -36,8 +35,8 @@ public class LowBarShootAutoAlignAutonomous extends CommandGroup {
         addSequential(new IntakeArmRaise());
         addSequential(new DriveRotateThetaWithGyro(15));
         //addSequential(new DriveFowardsForTime(1));
-        addSequential(new RoutineAutoAlignRPMs());
-        addSequential(new RoutineAutoAlignRPMs());
+        addSequential(new RoutineAutoAlign());
+        addSequential(new RoutineAutoAlign());
         //addSequential(new RoutineAutoAlign());
         addSequential(new ShooterWaitForStabilize());
         addSequential(new StageTwoQueueToShooter());
